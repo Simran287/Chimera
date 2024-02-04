8fe085e81139e08adfd6026647f9522a0357870b
Simran Bodhak <simranbodhak@microsoft.com>
2024-02-05 04:45:56 +0530

    day1

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.getElementById("count-el").innerText = 9
+let count = 0;
+console.log(count);
+// console.log(myAge);
+let myAge = 24;
+console.log(myAge);
+//My age in dog years
+let humanToDog = 7;
+let myDogAge = myAge * humanToDog;
+console.log(myDogAge);
