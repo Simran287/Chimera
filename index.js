1d07e435ad023abe9380320c52c75fd1a045fc97
Simran Bodhak <simranbodhak@microsoft.com>
2024-03-05 05:24:11 +0530

    4-march

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // document.getElementById("count-el").innerText = 9
 let count = 0;
-console.log(count);
+// console.log(count);
+// // console.log(myAge);
+// let myAge = 24;
 // console.log(myAge);
-let myAge = 24;
-console.log(myAge);
-//My age in dog years
-let humanToDog = 7;
-let myDogAge = myAge * humanToDog;
-console.log(myDogAge);
+// //My age in dog years
+// let humanToDog = 7;
+// let myDogAge = myAge * humanToDog;
+// console.log(myDogAge);
+// count = 88;
+// console.log(count);
+// count = 100;
+// console.log(count);
+let bonusPoints = 50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
