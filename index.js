599525e2d06f168d662b2f21a955d89db527f493
Simran Bodhak <simranbodhak@microsoft.com>
2024-03-05 05:53:00 +0530

    increment

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-// document.getElementById("count-el").innerText = 9
+document.getElementById("count-el").innerText = 0
 let count = 0;
-// console.log(count);
-// // console.log(myAge);
-// let myAge = 24;
-// console.log(myAge);
-// //My age in dog years
-// let humanToDog = 7;
-// let myDogAge = myAge * humanToDog;
-// console.log(myDogAge);
-// count = 88;
-// console.log(count);
-// count = 100;
-// console.log(count);
-let bonusPoints = 50;
-console.log(bonusPoints);
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints);
-bonusPoints = bonusPoints - 75;
-console.log(bonusPoints);
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
+
+function increment() {
+    // console.log("Button was clicked");
+    count = count + 1;
+    // console.log(count);
+    document.getElementById("count-el").innerText = count;
+}
