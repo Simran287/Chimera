ee93285fae9ce296556f9ed610fed7ae185f176f
Simran Bodhak <simranbodhak@microsoft.com>
2024-03-10 06:27:26 +0530

    completed count

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0;
 
 function increment() {
     // console.log("Button was clicked");
     count += 1;
     // console.log(count);
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 function save() {
-    console.log(count);
+    let entries = " " + count + " - ";
+    saveEl.textContent += entries;
+    count = 0;
+    countEl.textContent = count;
 }
+
+// count = 0;
 
 let welcomeEl = document.getElementById("welcome-el")
 let username = "Sadie"
